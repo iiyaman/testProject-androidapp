1c3c79181496681d5ac8cd00645a0118e348fca8
iiyaman <n.iyama0709@gmail.com>
2016-12-14 15:54:55 +0100

    Add 主要ファイルの説明に追加

--- app/src/main/java/com/example/iiyamanaoki/testproject/MainActivity.java
+++ app/src/main/java/com/example/iiyamanaoki/testproject/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.iiyamanaoki.testproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//アプリ起動に呼び出される画面のクラス
+//画面レイアウトファイル res/layout/activity_main.xml
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //画面が作成されるときに呼ばれるメソッド
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
